ff2574600bc47fd65b6657032f4280192f536a1e
Slymey <68031673+Slymey@users.noreply.github.com>
2023-12-04 09:42:49 +0100

    Updated Range.java

--- Range.java
+++ Range.java
@@ -8,24 +8,46 @@ import java.util.Iterator;
 public class Range implements Iterable<Integer>, Iterator<Integer>, Comparable<Integer>{
     int min=0;
     int max=0;
+    int num=0;
     private Iterator itr=null;
     private int current=0;
+    private int step =1;
     public Range(int min, int max){
+        if(min>max){this.step=-1;}
         this.min=min;
         this.max=max;
         current=min;
     }
+    public Range(int min, int num, int step){
+        this.min=min;
+        this.num=num;
+        this.step=step;
+        current=min;
+        this.max=min+num*step;
+        if(num<1){
+            this.min=0;
+            this.max=0;
+            this.step=0;
+        }
+    }
 
     @Override
     public boolean hasNext() {
-        boolean b=current<=max;
+        boolean b=false;
+        if(step<0){
+            b=current>=max;
+        }else{
+            b=current<=max;
+        }
         if(!b)current=min;
         return b;
     }
 
     @Override
     public Integer next() {
-        return current++;
+        int ret=current;
+        current+=step;
+        return ret;
     }
 
     @Override
@@ -44,4 +66,13 @@ public class Range implements Iterable<Integer>, Iterator<Integer>, Comparable<I
     public boolean equals(Integer i) {
         return this.compareTo(i)==0;
     }
+    
+    public int random(){
+        return (int)(Math.random()*(max-min+1)+min);
+    }
+    
+    public static int random(int min, int max){
+        return (int)(Math.random()*(max-min+1)+min);
+    }
+
 }
